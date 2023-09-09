632e9cb49d07c24e6f39cfed5a4398fa2ea3c2cd
salama-alkaabi <sal611592@gmail.com>
2023-09-09 10:11:31 +0400

    Initial Commit

---
+++ one/oneApp.swift
@@ -0,0 +1,17 @@
+//
+//  oneApp.swift
+//  one
+//
+//  Created by sal on 09/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct oneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
